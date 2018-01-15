2fba472b8f0ca328ceaf54b6f58eb93e183df9cc
minderwertig <hubzein@gmail.com>
2018-01-15 17:14:29 +0500

    fix tabulation

--- stack.js
+++ stack.js
@@ -1,37 +1,37 @@
 class Stack {
-  constructor(capacity) {
-    this._storage = {}
-    this._capacity = capacity || Infinity
-    this._count = 0
-  }
+	constructor(capacity) {
+		this._storage = {}
+		this._capacity = capacity || Infinity
+		this._count = 0
+	}
 
-  push(value) {
-    if(this._count < this._capacity) {
-      this._storage[this._count++] = value
-      return this._count
-    }
-    return 'Max capacity already reached. Remove element before adding a new one.'
-  }
+	push(value) {
+		if(this._count < this._capacity) {
+			this._storage[this._count++] = value
+			return this._count
+		}
+		return 'Max capacity already reached. Remove element before adding a new one.'
+	}
 
-  pop() {
-    if(this.count == 0)
-      return 'No element inside the stack. Add element before poping.'
-    var value = this._storage[--this.count]
-    delete this._storage[this._count]
-    return value;
-  }
+	pop() {
+		if(this.count == 0)
+			return 'No element inside the stack. Add element before poping.'
+		var value = this._storage[--this.count]
+		delete this._storage[this._count]
+		return value;
+	}
 
-  peek() {
-    return this._storage[this._count - 1]
-  }
+	peek() {
+		return this._storage[this._count - 1]
+	}
 
-  count() {
-    return this._count;
-  }
+	count() {
+		return this._count;
+	}
 
-  until(value) {
-    return Object.values(this._storage).reverse().indexOf(value)
-  }
+	until(value) {
+		return Object.values(this._storage).reverse().indexOf(value)
+	}
 }
 
 export default Stack
