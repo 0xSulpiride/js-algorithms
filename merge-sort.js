c2c1ecb635073a98558ffa62ad57d03718bb2fc8
minderwertig <hubzein@gmail.com>
2018-01-19 20:12:44 +0500

    iterative merge sort

--- merge-sort.js
+++ merge-sort.js
@@ -3,7 +3,7 @@ function merge(lstl, lstr) {
 	while(lstl.length > 0 && lstr.length > 0) {
 		lst.push((lstl[0] > lstr[0] ? lstr : lstl).shift())
 	}
-	if(lstl.length > 0)
+	if(lstl.length > 0) 
 		lst.push(...lstl);
 	if(lstr.length > 0)
 		lst.push(...lstr);
@@ -17,4 +17,19 @@ function mergeSort(lst) {
 	let lstl = mergeSort(lst.slice(0, mid));
 	let lstr = mergeSort(lst.slice(mid));
 	return merge(lstl, lstr);
+}
+
+function mergeSortIterative(lst) {
+	let arr = lst.map(elem => [elem]);
+	while(arr.length > 1) {
+		let result = [];
+		for(let i = 0; i < arr.length; i +=2) {
+			if(arr[i + 1])
+				result.push(merge(arr[i], arr[i+1]));
+			else
+				result.push(arr[i]);
+		}
+		arr = result;
+	}
+	return arr[0];
 }
